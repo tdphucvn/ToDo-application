2b7f43ea91a06a7758d5bc9eab52816385c4400b
Philip Tran <tran.philip02@gmail.com>
2020-11-19 01:12:11 +0100

    Init Project

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,48 @@
+//Loading .env file just in developer enviroment
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config({path: '.env'});
+};
+
+//import expressjs
 import express, {Application, Request, Response, NextFunction} from 'express';
+import expressLayouts from 'express-ejs-layouts';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
 
 const app: Application = express();
 
-const add = (a: number, b: number): number => a+b;
+// import {router as indexRouter} from './routes/index';
 
-app.get('/', (req: Request, res: Response) => {
-    console.log(add(4, 9));
-    res.send('Hello World');
-});
+import indexRouter = require('./routes/index');
 
-app.listen(5000, () => console.log('Server running'));
+//Using EJS as view engine
+app.set('view engine', 'ejs');
+//directory where the template files are located
+app.set('views', __dirname + '/../views');
+//setting the default view
+app.set('layout', __dirname + '/../views/layouts/layout');
+app.use(expressLayouts);
+app.use(methodOverride('_method'));
+//Public files CSS/JS
+app.use(express.static('public'));
+//Parsing to JSON data
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
+
+
+//Middleware routes
+app.use('/', indexRouter);
+
+
+
+try{
+    if(process.env.DB_CONNECTION == undefined){
+        console.log('uri is undefined');
+    }else{
+        mongoose.connect(process.env.DB_CONNECTION, {useUnifiedTopology: true , useNewUrlParser: true}, () => console.log('Success Connection'));
+    }
+}catch (err){
+    console.log('Access denied');
+};
+
+app.listen(process.env.PORT || 5000);
